a9940bc358b3ce2c7ba3dc10255777f01d9baa95
Brody Bayley <brodybayley@gmail.com>
2021-02-11 01:30:34 +0000

    adding word search and test file. Worked with Anastasia on this

--- wordsearch.js
+++ wordsearch.js
@@ -1,7 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    //Horizontal stuff:
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    //Vertical Stuff:
+    const transpose = function (matrix) {
+        let newArr = [];
+        for (let i = 0; i < matrix[0].length; i++) {
+            let tempArr = [];
+            for (let j = 0; j < matrix.length; j++) {
+                tempArr.push(matrix[j][i]);
+            }
+            newArr.push(tempArr.join(''));
+        }
+        return newArr;
+    };
+    let vertical = transpose(letters);
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    for (l of vertical) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    if (word === '') {
+        return undefined;
+    } else {
+        return false;
     }
 }
 
